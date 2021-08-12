232dd3412e5e8c5c44c1e08a20a32e034b2279da
eduluca <78570829+eduluca@users.noreply.github.com>
2021-08-12 14:28:28 -0400

    Adding new Xcode file

---
+++ ETtracker Xcode/ETtracker/PoseViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PoseViewController.swift
+//  ETtracker
+//
+//  Created by Sean Hall on 11/11/20.
+//
+
+import UIKit
+import CoreMotion
+import Messages
+
+class PoseViewController: UIViewController  {
+
+    @IBOutlet weak var graphView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
